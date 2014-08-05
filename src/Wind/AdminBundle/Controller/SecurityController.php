5964aab9c6a71c8be5f27c17fd83f8ed87124c96
matvey <mykola.mateychuk@internetdevels.com>
2014-08-05 14:03:16 +0300

    Add crud for Admin Author

--- src/Wind/AdminBundle/Controller/SecurityController.php
+++ src/Wind/AdminBundle/Controller/SecurityController.php
@@ -7,6 +7,7 @@ namespace Wind\AdminBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Security\Core\SecurityContext;
 
 class SecurityController extends Controller {
