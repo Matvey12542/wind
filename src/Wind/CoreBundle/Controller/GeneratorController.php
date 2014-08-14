197d2d77792102e5147a852def55efb725d15c0f
matvey <mykola.mateychuk@internetdevels.com>
2014-08-14 14:02:09 +0300

    Added upload files

--- src/Wind/CoreBundle/Controller/GeneratorController.php
+++ src/Wind/CoreBundle/Controller/GeneratorController.php
@@ -5,6 +5,8 @@ namespace Wind\CoreBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Wind\ModelBundle\Entity\Generator;
 
 /**
  * Class GeneratorController
@@ -23,10 +25,49 @@ class GeneratorController extends Controller
             'generators' =>  $generators
         );
     }
+
+    /**
+     * @Route("/upload")
+     * @Template()
+     */
+    public function uploadAction(Request $request)
+    {
+        $generator = new Generator();
+        $form = $this->createFormBuilder($generator)
+            ->add('title')
+            ->add('file')
+            ->add('body')
+            ->add('performance')
+            ->add('author')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            $generator->upload();
+            $em->persist($generator);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('/'));
+        }
+
+        return array('form' => $form->createView());
+    }
+
     /**
      * @return object
      */
     private function getGeneratorManager() {
         return $this->get('generatorManager');
+    }
+
+    /**
+     * Login check
+     *
+     * @Route("upload_check")
+     */
+    public function uploadCheckAction() {
     }
 }
