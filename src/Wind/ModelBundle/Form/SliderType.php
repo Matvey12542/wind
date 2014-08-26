85d6a5fd2e29f03679a21054a52971c9d5197dd7
Kolya <Matvey12542@rambler.ru>
2014-08-26 23:58:33 +0300

    Add admin for generator

---
+++ src/Wind/ModelBundle/Form/SliderType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Wind\ModelBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SliderType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('file')
+            ->add('body')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Wind\ModelBundle\Entity\Slider'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'wind_modelbundle_slider';
+    }
+}
