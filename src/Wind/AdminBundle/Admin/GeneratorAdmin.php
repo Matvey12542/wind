fe0535e0e17f84bde74dffd25f687cbd5860b26a
matvey <mykola.mateychuk@internetdevels.com>
2014-09-22 13:12:58 +0300

    add admin servise

---
+++ src/Wind/AdminBundle/Admin/GeneratorAdmin.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kolya
+ * Date: 12.09.14
+ * Time: 17:38
+ */
+
+namespace Wind\AdminBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\CoreBundle\Form\Type;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+use Wind\ModelBundle\Entity\Generator;
+class GeneratorAdmin extends Admin {
+    /**
+     * @param \Sonata\AdminBundle\Form\FormMapper $formMapper
+     *
+     * @return void
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+
+            ->add('title')
+            ->add('file', 'file')
+            ->add('body')
+            ->add('performance')
+            ->add('initialSpeed')
+            ->add('ratedSpeed')
+            ->add('weight')
+            ->add('price')
+            ->add('author')
+//            ->add('tags')
+        ;
+    }
+
+    /**
+     * @param \Sonata\AdminBundle\Show\ShowMapper $showMapper
+     *
+     * @return void
+     */
+    protected function configureShowField(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('title')
+            ->add('file', 'file')
+            ->add('body')
+            ->add('performance')
+            ->add('initialSpeed')
+            ->add('ratedSpeed')
+            ->add('weight')
+            ->add('price')
+            ->add('author')
+//            ->add('image')
+        ;
+    }
+
+    /**
+     * @param \Sonata\AdminBundle\Datagrid\ListMapper $listMapper
+     *
+     * @return void
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('title')
+            ->add('file', 'file')
+            ->add('body')
+            ->add('performance')
+            ->add('initialSpeed')
+            ->add('ratedSpeed')
+            ->add('weight')
+            ->add('price')
+            ->add('author')
+//            ->add('image')
+//            ->add('tags')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+    /**
+     * @param \Sonata\AdminBundle\Datagrid\DatagridMapper $datagridMapper
+     *
+     * @return void
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('title')
+//            ->add('file', 'file')
+            ->add('body')
+            ->add('performance')
+            ->add('initialSpeed')
+            ->add('ratedSpeed')
+            ->add('weight')
+            ->add('price')
+            ->add('author')
+//            ->add('image')
+        ;
+    }
+    // setup the default sort column and order
+    protected $datagridValues = array(
+        '_sort_order' => 'ASC',
+        '_sort_by' => 'name'
+    );
+} 
