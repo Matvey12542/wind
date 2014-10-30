3216298a80be82ec0bb400ee8163545ed5c21d41
Kolya <Matvey12542@rambler.ru>
2014-10-30 22:53:58 +0200

    bad commit

--- src/Wind/ModelBundle/Entity/Post.php
+++ src/Wind/ModelBundle/Entity/Post.php
@@ -7,22 +7,25 @@ use Gedmo\Translatable\Translatable;
 use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\Common\Collections\ArrayCollection;
 use Gedmo\Mapping\Annotation as Gedmo;
+//use Wind\ModelBundle\Entity\PostTranslation;
+
 /**
  * Post
  *
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="Wind\ModelBundle\Repository\PostRepository")
+ * @Gedmo\TranslationEntity(class="Wind\ModelBundle\Entity\PostTranslation")
  */
 class Post extends Timestampable implements Translatable
 {
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
-    private $id;
+  /**
+   * @var integer
+   *
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
 
   /**
    * @ORM\Column(name="ontop", type="boolean", nullable=true)
@@ -30,67 +33,90 @@ class Post extends Timestampable implements Translatable
    */
   private $ontop = false;
 
-    /**
-     * @var string
-     * @Gedmo\Translatable()
-     * @ORM\Column(name="title", type="string", length=150)
-     * @Assert\NotBlank()
-     */
-    private $title;
+  /**
+   * @var string
+   * @Gedmo\Translatable()
+   * @ORM\Column(name="title", type="string", length=150)
+   * @Assert\NotBlank()
+   */
+  private $title;
 
-    /**
-     * @var string
-     *
-     * @Gedmo\Slug(fields={"title"}, unique=false)
-     * @ORM\Column(length=255)
-     */
-    private $slug;
-    /**
-     * @var string
-     * @Gedmo\Translatable()
-     * @ORM\Column(name="body", type="text")
-     */
-    private $body;
+  /**
+   * @var string
+   *
+   * @Gedmo\Slug(fields={"title"}, unique=false)
+   * @ORM\Column(length=255)
+   */
+  private $slug;
+  /**
+   * @var string
+   * @Gedmo\Translatable()
+   * @ORM\Column(name="body", type="text")
+   */
+  private $body;
 
-    /**
-     * @var Author
-     *
-     * @ORM\ManyToOne(targetEntity="Author", inversedBy="posts")
-     * @ORM\JoinColumn(name="author_id", referencedColumnName="id", nullable=false)
-     * @Assert\NotBlank()
-     */
-    private $author;
+  /**
+   * @var Author
+   *
+   * @ORM\ManyToOne(targetEntity="Author", inversedBy="posts")
+   * @ORM\JoinColumn(name="author_id", referencedColumnName="id", nullable=false)
+   * @Assert\NotBlank()
+   */
+  private $author;
 
-    /**
-     * @var ArrayCollection
-     *
-     * @ORM\OneToMany(targetEntity="Comment", mappedBy="post", cascade={"remove"})
-     */
-    private $comments;
+  /**
+   * @var ArrayCollection
+   *
+   * @ORM\OneToMany(targetEntity="Comment", mappedBy="post", cascade={"remove"})
+   */
+  private $comments;
 
-    /**
-     * @Gedmo\Locale()
-     */
-    private $locale;
+  /**
+   * @Gedmo\Locale()
+   */
+//    private $locale;
 
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        $this->comments = new ArrayCollection();
+  /**
+   * @ORM\OneToMany(
+   *   targetEntity="PostTranslation",
+   *   mappedBy="object",
+   *   cascade={"persist", "remove"}
+   * )
+   */
+  private $translations;
+
+  /**
+   * Constructor
+   */
+  public function __construct()
+  {
+    $this->comments = new ArrayCollection();
+    $this->translations = new ArrayCollection();
 //      $this->ontop = false;
-    }
+  }
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
-        return $this->id;
+  public function getTranslations()
+  {
+    return $this->translations;
+  }
+
+  public function addTranslation(PostTranslation $t)
+  {
+    if (!$this->translations->contains($t)) {
+      $this->translations[] = $t;
+      $t->setObject($this);
     }
+  }
+
+  /**
+   * Get id
+   *
+   * @return integer
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
 
   /**
    * Set ontop
@@ -114,133 +140,135 @@ class Post extends Timestampable implements Translatable
   {
     return $this->ontop;
   }
-    /**
-     * Set title
-     *
-     * @param string $title
-     * @return Post
-     */
-    public function setTitle($title)
-    {
-        $this->title = $title;
-    
-        return $this;
-    }
 
-    /**
-     * Get title
-     *
-     * @return string 
-     */
-    public function getTitle()
-    {
-        return $this->title;
-    }
+  /**
+   * Set title
+   *
+   * @param string $title
+   * @return Post
+   */
+  public function setTitle($title)
+  {
+    $this->title = $title;
 
-    /**
-     * Set slug
-     *
-     * @param string $slug
-     * @return Post
-     */
-    public function setSlug($slug)
-    {
-        $this->slug = $slug;
+    return $this;
+  }
 
-        return $this;
-    }
+  /**
+   * Get title
+   *
+   * @return string
+   */
+  public function getTitle()
+  {
+    return $this->title;
+  }
 
-    /**
-     * Get slug
-     *
-     * @return string
-     */
-    public function getSlug()
-    {
-        return $this->slug;
-    }
+  /**
+   * Set slug
+   *
+   * @param string $slug
+   * @return Post
+   */
+  public function setSlug($slug)
+  {
+    $this->slug = $slug;
 
-    /**
-     * Set body
-     *
-     * @param string $body
-     * @return Post
-     */
-    public function setBody($body)
-    {
-        $this->body = $body;
-    
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * Get body
-     *
-     * @return string 
-     */
-    public function getBody()
-    {
-        return $this->body;
-    }
+  /**
+   * Get slug
+   *
+   * @return string
+   */
+  public function getSlug()
+  {
+    return $this->slug;
+  }
+
+  /**
+   * Set body
+   *
+   * @param string $body
+   * @return Post
+   */
+  public function setBody($body)
+  {
+    $this->body = $body;
+
+    return $this;
+  }
+
+  /**
+   * Get body
+   *
+   * @return string
+   */
+  public function getBody()
+  {
+    return $this->body;
+  }
 
 
-    /**
-     * Set author
-     *
-     * @param \Wind\ModelBundle\Entity\Author $author
-     * @return Post
-     */
-    public function setAuthor(Author $author)
-    {
-        $this->author = $author;
-    
-        return $this;
-    }
+  /**
+   * Set author
+   *
+   * @param \Wind\ModelBundle\Entity\Author $author
+   * @return Post
+   */
+  public function setAuthor(Author $author)
+  {
+    $this->author = $author;
 
-    /**
-     * Get author
-     *
-     * @return \Wind\ModelBundle\Entity\Author 
-     */
-    public function getAuthor()
-    {
-        return $this->author;
-    }
+    return $this;
+  }
 
-    /**
-     * Add comments
-     *
-     * @param \Wind\ModelBundle\Entity\Comment $comments
-     * @return Post
-     */
-    public function addComment(Comment $comments)
-    {
-        $this->comments[] = $comments;
-    
-        return $this;
-    }
+  /**
+   * Get author
+   *
+   * @return \Wind\ModelBundle\Entity\Author
+   */
+  public function getAuthor()
+  {
+    return $this->author;
+  }
 
-    /**
-     * Remove comments
-     *
-     * @param \Wind\ModelBundle\Entity\Comment $comments
-     */
-    public function removeComment(Comment $comments)
-    {
-        $this->comments->removeElement($comments);
-    }
+  /**
+   * Add comments
+   *
+   * @param \Wind\ModelBundle\Entity\Comment $comments
+   * @return Post
+   */
+  public function addComment(Comment $comments)
+  {
+    $this->comments[] = $comments;
 
-    /**
-     * Get comments
-     *
-     * @return \Doctrine\Common\Collections\Collection 
-     */
-    public function getComments()
-    {
-        return $this->comments;
-    }
+    return $this;
+  }
 
-    public function setTranslatableLocale($locale) {
-        $this->locale = $locale;
-    }
+  /**
+   * Remove comments
+   *
+   * @param \Wind\ModelBundle\Entity\Comment $comments
+   */
+  public function removeComment(Comment $comments)
+  {
+    $this->comments->removeElement($comments);
+  }
+
+  /**
+   * Get comments
+   *
+   * @return \Doctrine\Common\Collections\Collection
+   */
+  public function getComments()
+  {
+    return $this->comments;
+  }
+
+  public function setTranslatableLocale($locale)
+  {
+    $this->locale = $locale;
+  }
 }
